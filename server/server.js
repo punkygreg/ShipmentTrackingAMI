c8d7d5e5d69cc4d594fe047db362381b23507bbc
punkygreg <156363881+punkygreg@users.noreply.github.com>
2026-03-03 20:19:46 +0000

    POST functional

--- server/server.js
+++ server/server.js
@@ -1,28 +1,70 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
-const controller = require('./controllers');
-const { Token } = require('acorn');
+const PORT = 3333;
+//const controller = require('./controllers');
+//const { Token } = require('acorn');
 
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-//API Call
-// app.get('./', controller.apiCall, (req, res) => {
-//   return res.status(200).send(res);
-// })
-const options = {method: 'GET', headers: {Authorization: 'Token MJM2Ler3McaDUHcErMxCqMVo'}};
+//Listing paramaters for API call
+const optionsP= {
+  method: 'POST',
+  headers: {Authorization: 'Token MJM2Ler3McaDUHcErMxCqMVo', 'Content-Type': 'application/json'},
+  body: JSON.stringify({
+    data: {
+      attributes: {
+        request_type: 'bill_of_lading',
+        request_number: 'CMDULHV3925105',
+        ref_numbers: ['8157US306035655', 'UA1710955', 'UA1710955'],
+        // shipment_tags: ['camembert'],
+        scac: 'CMDU'
+      },
+      // relationships: {customer: {data: {id: 'f7cb530a-9e60-412c-a5bc-205a2f34ba54', type: 'party'}}},
+      type: 'tracking_request'
+    }
+  })
+};
 
-//API direct CALL
-fetch('https://api.terminal49.com/v2/tracking_requests', options)
+//API CALL
+fetch('https://api.terminal49.com/v2/tracking_requests', optionsP)
+.then(res => res.json())
+.then(res => console.log(res))
+.then(console.log(("POD ETA:", data.data.attributes.pod_eta_at)))
+.catch(err => console.error(err));
+
+//GET ETA
+const optionsG =  {
+  method: 'GET',
+  headers: {Authorization: 'Token MJM2Ler3McaDUHcErMxCqMVo', 'Content-Type': 'application/vnd.api+json'},
+  // body: JSON.stringify({
+  //   data: {
+  //     attributes: {
+  //       pod_eta_at,
+  //       pod_original_eta_at,
+  //       destination_eta_at
+
+  //     },
+  //     // relationships: {customer: {data: {id: 'f7cb530a-9e60-412c-a5bc-205a2f34ba54', type: 'party'}}},
+  //     type: 'tracking_request'
+  //   }
+  // })
+}
+
+fetch('https://api.terminal49.com/v2/shipments/3c6eea72-0897-4de2-8e98-ecbb63420172', optionsG)
 .then(res => res.json())
 .then(res => console.log(res))
 .catch(err => console.error(err));
 
 
 
+
+
+
+
 // global error handler
+
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
